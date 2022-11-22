054b65daacf62a62bb49e36cbed84d58cb665a75
Degisew <jegisew21@gmail.com>
2022-11-22 16:43:01 +0300

    created a NavBar component

---
+++ src/components/Navbar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AppBar, Toolbar } from '@mui/material';
+
+const NavBar = () => (
+  <header>
+    <AppBar position="static">
+      <Toolbar variant="dense" />
+    </AppBar>
+  </header>
+);
+
+export default NavBar;
