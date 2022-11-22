e3055e13b408e562d00634d364a5ab31bc8eac26
Degisew <jegisew21@gmail.com>
2022-11-22 22:35:50 +0300

    add a style

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Toolbar, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import styles from './styles';
 import logo from '../../assets/images/planet.png';
@@ -9,7 +9,7 @@ const NavBar = () => (
     <AppBar position="static" sx={styles.appbar}>
       <div className="logo">
         <img src={logo} alt="logo" />
-        <Typography variant="h5">Space Travelers&apos; Hub</Typography>
+        <Typography variant="h5" sx={styles.logoTitle}>Space Travelers&apos; Hub</Typography>
       </div>
       <Toolbar variant="dense" sx={styles.toolbar}>
         <Link to="/" className="link">
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,14 +1,20 @@
 const styles = {
   appbar: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    backgroundColor: "silver",
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'silver',
   },
   toolbar: {
-    display: "flex",
-    alignSelf: "flex-end",
+    display: 'flex',
+    alignSelf: 'flex-end',
+  },
+  logoTitle: {
+    display: {
+      xs: 'none',
+      md: 'flex',
+    },
   },
 };
 
