234640abb3e0eb63aecb66869a93bdfd7223c663
Degisew <jegisew21@gmail.com>
2022-11-22 20:55:29 +0300

    Add a logo

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { AppBar, Toolbar } from '@mui/material';
+import { AppBar, Toolbar, Typography } from "@mui/material";
 import { Link } from 'react-router-dom';
 import styles from './styles';
+import logo from '../../assets/images/planet.png';
 
 const NavBar = () => (
   <>
     <AppBar position="static" sx={styles.appbar}>
+      <div className="logo">
+        <img src={logo} alt="logo" />
+        <Typography variant="h5">Space Travelers&apos; Hub</Typography>
+      </div>
       <Toolbar variant="dense" sx={styles.toolbar}>
         <Link to="/" className="link">
           Rockets
