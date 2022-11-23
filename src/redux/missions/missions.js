628877f13e7aed21e035e007879c9d5fd7b16d76
Olivier-Kango <olivierkango@gmail.com>
2022-11-23 15:37:18 -0800

    fetch missions from API with createAsyncThunk

---
+++ src/redux/missions/missions.js
@@ -0,0 +1,25 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import getMissionsApi from '../../api/SpaceApi';
+
+const LOAD_MISSIONS = 'LOAD_MISSIONS';
+
+// LOAD
+export const loadMissions = createAsyncThunk(LOAD_MISSIONS, async () => {
+  try {
+    const response = await getMissionsApi();
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+// SPACE STORE
+const MissionsSlice = createSlice({
+  name: 'missionsstore',
+  initialState: {},
+  extraReducers: (builder) => {
+    builder.addCase(loadMissions.fulfilled, (state, action) => action.payload);
+  },
+});
+
+export default MissionsSlice;
