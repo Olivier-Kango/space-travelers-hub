bb0ebefa9bf2656f917cc5534ef74dc756138898
Degisew <jegisew21@gmail.com>
2022-11-24 22:44:50 +0300

    display a reserved rockets on myProfile page

--- src/components/MyProfile/MyProfile.jsx
+++ src/components/MyProfile/MyProfile.jsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import React from "react";
+import {
+  Container,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { useSelector } from "react-redux";
 
-const MyProfile = () => (
-  <div>
-    MyProfile
-  </div>
-);
+const myprofile = () => {
+const rockets = useSelector(state => state.rockets);
+  return (
+    <section>
+      <Container maxWidth="lg" sx={{ display: "flex", columnGap: "20px" }}>
+        <Container>
+          <Typography variant="h5">My Missions</Typography>
+          <Table sx={{ border: "0.5px solid black" }}>
+            <TableBody>
+              {rockets.map((rocket) => {
+                return (
+                  rocket.reserved && (
+                    <TableRow key={rocket.id}>
+                      <TableCell key={rocket.id}>{rocket.name}</TableCell>
+                    </TableRow>
+                  )
+                );
+              })}
+            </TableBody>
+          </Table>
+        </Container>
+        <Container>
+          <Typography variant="h5">My Rockets</Typography>
+          <Table sx={{ border: "0.5px solid black" }}>
+            <TableBody>
+              {rockets.map((rocket) => {
+                return (
+                  rocket.reserved && (
+                    <TableRow key={rocket.id}>
+                      <TableCell key={rocket.id}>{rocket.name}</TableCell>
+                    </TableRow>
+                  )
+                );
+              })}
+            </TableBody>
+          </Table>
+        </Container>
+      </Container>
+    </section>
+  );
+};
 
-export default MyProfile;
+export default myprofile;
