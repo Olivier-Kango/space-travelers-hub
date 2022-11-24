6a7fbb6fc8d7f113a75829c72ee99f820a187b18
Olivier-Kango <olivierkango@gmail.com>
2022-11-24 07:01:02 -0800

    Dispatch the action for reservedMission

--- src/components/Missions/Mission.jsx
+++ src/components/Missions/Mission.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { reservedMission } from '../../redux/missions/reservedMission';
 import './Missions.css';
 
+let reserved = false;
 const Mission = (mission) => {
   const { id, name, description } = mission;
+  const status = useSelector((state) => state.reducerReservedMission);
+  const dispatch = useDispatch();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    reserved = !reserved;
+    if (reserved) {
+      e.target.classList.add('change');
+      return dispatch(reservedMission('Leave Mission'));
+    }
+    e.target.classList.remove('change');
+    return dispatch(reservedMission('Join Mission'));
+  };
 
   return (
     <div key={name} id={id} className="mission">
       <h3>{name}</h3>
       <p>{description}</p>
       <div className="member"><button type="button">NOT A MEMBER</button></div>
-      <div className="join"><button type="button">Join Mission</button></div>
+      <div className="join">
+        <button
+          type="button"
+          id={`button-${id}`}
+          onClick={(e) => handleClick(e)}
+        >
+          {status}
+        </button>
+      </div>
     </div>
   );
 };
