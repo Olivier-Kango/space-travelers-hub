a0973f490d408e7ab07dd56796243c901cd7a016
Olivier-Kango <olivierkango@gmail.com>
2022-11-24 18:39:25 -0800

    Remove the console.log

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,10 @@
 import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import MissionsSlice from './missions/missions';
-import reducerReservedMission from './missions/reservedMission';
 
 const rootReducer = {
   reducer: {
     missions: MissionsSlice.reducer,
-    reducerReservedMission,
   },
 };
 
