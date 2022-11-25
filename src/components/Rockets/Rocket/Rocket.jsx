faf7ef1922ea6b3a56ca6655ee076c384ab0d089
Olivier-Kango <olivierkango@gmail.com>
2022-11-24 22:33:26 -0800

    fix linter errors for object.Prototype

--- src/components/Rockets/Rocket/Rocket.jsx
+++ src/components/Rockets/Rocket/Rocket.jsx
@@ -59,7 +59,7 @@ const Rocket = ({ rocket }) => {
 };
 
 Rocket.propTypes = {
-  rocket: PropTypes.objectOf(PropTypes.any).isRequired,
+  rocket: PropTypes.objectOf(PropTypes.object()),
   image: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
